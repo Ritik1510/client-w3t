3b42ce9215dfdff36ef4521ebce7a4e8e9c254f3
ritik kumar <kr.ritik1510@gmail.com>
2025-09-10 02:19:31 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="outer-main">
+        <div className='inner-main'>
+          <h1 className='heading-text'>Apple Intelligence</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
