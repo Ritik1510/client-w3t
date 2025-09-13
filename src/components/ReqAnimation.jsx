0436ad9004d0bf6bbacb6f790399141237728c89
ritik kumar <kr.ritik1510@gmail.com>
2025-09-13 11:41:17 +0530

    improve explanation and move the position of connection status text

--- src/components/ReqAnimation.jsx
+++ src/components/ReqAnimation.jsx
@@ -3,20 +3,47 @@ import "../index.css";
 import { attemptReconnect } from "../reconnectionHelpers/attemptReconnect";
 
 /**
- * 1. check the connection for already open or not "avoids creating duplicate connections."
+ * 1. check the connection for already open or not "for avoiding the creation of duplicate connections."
  * 2. Establishes a new WebSocket connection to your server at ws://localhost:8000
- * 3. for tracking how much time the web socket connection is lived,
- * declare a variable that stores the current timestamp
- * 4. 'socket.onpen' means client is connected to the opened ws connection and
- * fires the initial connection logic with that --> inform the client with current status --> clearing errors on success
- * 5. 'socket.onmessage' this event is fire when the client ws connection recieves message from the server and try/catch for something, here or typically the recieved message is parsed and take use of it to update the ui or anything else,
- * for now, we parse the message ---> inform the user/client ---> checks the recieved response formate and its values ---> based on that continue the corresponding actions ---> if the try/catch fails then inform the user again
- * 6. 'socket.onclose' this event is fires when the server is closed, on closing event from the either both sides client and server ---> imdideately inform the user/client ---> then update the necessary component based states ---> check for the timestamp is recieved or not if avilable then procced the corresponding actions ---> like clacluate the time in seconds and inform to the user/client.
- * 7. 'socket.onerror' this event fires if there’s a network issue or unexpected socket based error ---> send a closing handshake to the server and relase the connected user/client based resources ---> inform the user/client with prefered message 'network issue' --->
- * 8. disconnectFromServer method ---> sockets current state like is it open or not ---> based on the that conditions, the corresponding tasks are executed ---> like sends the message to the server that the client is disconnected or intentionlly disconnect, so the server takes the further action on stored client resouces like relase the client information completely and lot more ---> send the closing handshake to the server.
- * 9. sendMessage method -==> first check the conection is open or not at the last rendering of component using socket instance reference variable ---> SEND THE JSON MESSAGE data TO THE SERVER SO SERVER RESPONSE BACK BASED ON SPECIFIED FLAGS NAMED AS ['start-animation', 'stop-animation'] UNDER CONDITION OF ['start', stop']---> AND THE CLIENT DECIDES WHAT TO DO WITH THAT RECIEVED MESSAGES FROM THE SERVER ---> like infroming the use/client for message is recieved by server.
- * 10.
- * **/
+ * 3. for tracking how much time the web socket connection is lived, declare a variable that stores the current timestamp
+ *
+ * 4. ['socket.onpen'] means client is connected to the opened ws connection [fully-duplex, hand-shaked, ready-to-send/recieve-request] and
+ * ---> fires the initial connection logic with that
+ * ---> inform the client with current status in status element
+ * ---> clearing errors on success
+ * ---> and reset the state of reconnectAttempt
+ *
+ * 5. ['socket.onmessage'] this event is fire when the client ws connection recieve messages from the server
+ * ---> try/catch for something, here or typically the recieved message is parsed and take use of it to update the ui or anything else in DOM tree, for now, we just parse the message
+ * ---> inform the user/client
+ * ---> checks the recieved response formate and its values
+ * ---> based on that continue the corresponding actions
+ * ---> if the try/catch fails for continuing the further taskes then inform the user again
+ *
+ * 6. ['socket.onclose'] this event is fires when the server is closed, on closing event from the either both sides client and server
+ * ---> imdideately inform the user/client
+ * ---> then update the necessary component based states
+ * ---> check for the timestamp is recieved or not if avilable then procced the corresponding actions
+ * ---> like clacluate the time in seconds and inform to the user/client.
+ *
+ * 7. ['socket.onerror'] this event fires if there’s a network issue or unexpected socket based error
+ * ---> send a closing handshake to the server and relase the connected user/client based resources
+ * ---> inform the user/client with prefered message 'network issue'
+ *
+ * 8. ]disconnectFromServer] method ===>
+ * ---> sockets current state like is it open or not
+ * ---> based on the that conditions, the corresponding tasks are executed
+ * ---> like sends a message to the server that the client is disconnected or intentionlly disconnect, so the server takes the further action on stored client resouces for example release the client information completely
+ * ---> send the closing handshake to the server.
+ *
+ * 9. [sendMessage] method ===>
+ * ---> first check the conection is open or not at the last rendering of component using socket instance reference variable
+ * ---> SEND THE JSON MESSAGE data TO THE SERVER SO SERVER RESPONSE BACK BASED ON SPECIFIED FLAGS NAMED AS['start-animation', 'stop-animation'] UNDER CONDITION OF ['start', stop']
+ * ---> AND THE CLIENT DECIDES WHAT TO DO WITH THAT RECIEVED MESSAGES FROM THE SERVER
+ * ---> like infroming the use/client for message is recieved by server.
+ * 10. [handleAnimationEnd] method ===>
+ * --->
+ * */
 
 export default function ReqAnimation() {
   const boxRef = useRef(null);
@@ -59,8 +86,8 @@ export default function ReqAnimation() {
 
         if (msg.type === "response") {
           if (msg.message === "start-animation") {
-            setShowBox(true); 
-            setAnimationStep("scale"); 
+            setShowBox(true);
+            setAnimationStep("scale");
             // Small timeout ensures DOM updates and animation restarts from step 1
             setTimeout(() => {
               setAnimationStep("scale");
@@ -80,8 +107,8 @@ export default function ReqAnimation() {
       console.log("⚠️ WebSocket disconnected");
       setShowBox(false);
       setConnected(false);
-      
-      if(!userDisconnected){
+
+      if (!userDisconnected) {
         attemptReconnect(
           reconnectAttempts,
           setReconnectAttempts,
@@ -106,7 +133,7 @@ export default function ReqAnimation() {
     // =========================================== on error ================================
     socket.onerror = (err) => {
       console.error("WebSocket error:", err);
-      setUserDisconnected(false)
+      setUserDisconnected(false);
       setConnected(false);
       socket.close();
     };
@@ -152,13 +179,15 @@ export default function ReqAnimation() {
         connectToServer,
         setError,
         userDisconnected,
+        setConnected,
       });
     };
 
     const handleOffline = () => {
-      console.warn("📴 Network went offline, pausing reconnection.");
+      console.warn("Network went offline, pausing reconnection.");
       setError("Network offline. Waiting...");
       setConnected(false);
+      setShowBox(false);
     };
 
     window.addEventListener("online", handleOnline);
@@ -180,47 +209,48 @@ export default function ReqAnimation() {
             onAnimationEnd={handleAnimationEnd}
           ></div>
         )}
-      </div>
-
-      <div className="heading-text-container">
-        <h1 className="heading-text">Apple Intelligence</h1>
-      </div>
-
-      <div className="connection-btns space-x-2 relative">
-        {!connected ? (
-          <button className="btn" onClick={connectToServer}>
-            Connect
-          </button>
-        ) : (
-          <button className="btn" onClick={disconnectFromServer}>
-            Disconnect
-          </button>
-        )}
-
-        <button
-          className={`btn ${!connected ? "btn-disabled" : ""}`}
-          onClick={() => sendMessage("start", "start-to-animation")}
-          disabled={!connected}
-        >
-          Start
-        </button>
-
-        <button
-          className={`btn ${!connected ? "btn-disabled" : ""}`}
-          onClick={() => sendMessage("stop", "stop-to-animation")}
-          disabled={!connected}
-        >
-          Stop
-        </button>
-
-        <div>
-          <span className="conn-status-box ml-3 ">
-            {connected ? "🟢 Connected" : "🔴 Disconnected"}
-          </span>
+        <div className="heading-text-container">
+          <h1 className="heading-text">Apple Intelligence</h1>
         </div>
       </div>
-      <div className="network-status-message absolute bottom-0 right-0 ">
-        {error && <p className="text-red-500">{error}</p>}
+
+      <div className="connection-btns">
+        <div className="conn-status-container">
+          {!connected ? (
+            <button className="btn" onClick={connectToServer}>
+              <span className="btn-text">Connect</span>
+            </button>
+          ) : (
+            <button className="btn" onClick={disconnectFromServer}>
+              <span className="btn-text">Disconnect</span>
+            </button>
+          )}
+          <div className="conn-status-box-container">
+            <span className="conn-status-box-text">
+              {connected ? "🟢 Connected" : "🔴 Disconnected"}
+            </span>
+          </div>
+        </div>
+
+        <div className="conn-start-stop-box">
+          <button
+            className={`btn ${!connected ? "btn-disabled" : ""}`}
+            onClick={() => sendMessage("start", "start-to-animation")}
+            disabled={!connected}
+          >
+            <span className="btn-text">Start</span>
+          </button>
+          <button
+            className={`btn ${!connected ? "btn-disabled" : ""}`}
+            onClick={() => sendMessage("stop", "stop-to-animation")}
+            disabled={!connected}
+          >
+            <span className="btn-text">Stop</span>
+          </button>
+        </div>
+      </div>
+      <div className="network-status-message">
+        {error && <p className="error-msg-text">{error}</p>}
       </div>
     </div>
   );
