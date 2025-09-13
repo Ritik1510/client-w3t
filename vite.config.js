cce10c83f3f3f6e43fb7252cb8471c3f8d53d777
ritik kumar <kr.ritik1510@gmail.com>
2025-09-13 11:39:06 +0530

    remove the unwanted commenting

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 5173, // your frontend port
+    port: 5173, 
     proxy: {
       // Proxy for WebSocket server
       "/ws": {
         target: "ws://localhost:8080",
-        ws: true, // <-- very important: enable WebSocket proxying
+        ws: true, // <-- enable WebSocket proxying
         changeOrigin: true, // avoid host header issues
-        secure: false, // if using self-signed certs (for dev)
+        secure: false, 
       },
 
-      // (optional) proxy for REST API if needed later
+      // proxy for REST API 
       "/api": {
         target: "http://localhost:3000", // Express REST API
         changeOrigin: true,
