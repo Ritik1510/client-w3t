8adee7bcf921cce1db20d52df75ceaf0d980832e
ritik1510 <kr.ritik1510@gmail.com>
2025-10-05 11:25:17 +0530

    adding the connecting spinner for managing the connecting ui behaviour

---
+++ src/components/ConnectingSpinner.jsx
@@ -0,0 +1,9 @@
+const ConnectingSpinner = () => {
+  return (
+    <div className="spinner-wrapper">
+      <div className="spinner"></div>
+    </div>
+  );
+}
+
+export default ConnectingSpinner; 
